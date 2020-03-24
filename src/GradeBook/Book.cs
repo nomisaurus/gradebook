c9722c22278e82d8ad0fd33d4128a3396fa1e1f7
Noman ul Haq <mn.ulhaq@gmail.com>
2020-03-24 14:26:58 -0400

    Add state and behavior

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     class Book
     {
+        List<double> grades;
 
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
     }
 }
